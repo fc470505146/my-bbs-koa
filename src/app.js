81b944df4468f5755c543f8ac77411ac1b4e9275
Fu C <2734927248@qq.com>
2021-12-12 12:17:46 +0800

    项目初始化

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const Koa=require('koa')
+
+const app=new Koa()
+
+
+
+
+module.exports=app
---
+++ src/main.js
@@ -0,0 +1,8 @@
+const app=require('./app')
+const {APP_PORT}=require('./config')
+
+
+app.listen(APP_PORT,()=>{
+    console.log(`server is running on
+     http://localhost:${APP_PORT}`);
+})
