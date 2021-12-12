b65882a238a5d0dca5fe53c1c1a6bb8de53f06c2
Fu C <2734927248@qq.com>
2021-12-12 12:21:16 +0800

    项目初始化

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
+const path=require('path')
 const Koa=require('koa')
-
+const serve=require('koa-static')
+const koa_body=require('koa-body')
 const app=new Koa()
-
+app.use(serve(path.resolve('public')))
+app.use(koa_body())
 
 
 
