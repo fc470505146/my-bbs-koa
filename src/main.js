5b7566dac3cda4b529002e5ddff1a85d26d4c79d
Fu C <2734927248@qq.com>
2022-02-12 11:36:47 +0800

    修复bug“hxbug

--- src/main.js
+++ src/main.js
@@ -6,3 +6,4 @@ app.listen(APP_PORT, () => {
      http://localhost:${APP_PORT}`)
 })
 
+//db.user.insertOne({ "_id" : ObjectId("61cfafc28de69ef19ee38020"), "username" : "root", "password" : "root", "nickname" : "超级管理员", "avatar" : "/img/976df8119e30fe74f7cfb8901.jpeg", "roles" : [ "admin" ] })
